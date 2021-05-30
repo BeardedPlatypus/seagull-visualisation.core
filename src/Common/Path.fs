a2a73cc71d2f829a6e5aab8cc8ced8eb07065209
BeardedPlatypus <mwtegelaers@gmail.com>
2021-05-30 12:06:42 +0200

    Add dependency on PathLib and create Path module

---
+++ src/Common/Path.fs
@@ -0,0 +1,12 @@
+﻿namespace Seagull.Visualisation.Core.Common
+
+open PathLib
+
+/// <summary>
+/// Provides some helper methods in addition to the PathLib library
+/// to ease development.
+/// </summary>
+module Path =
+    let public (/) (left: IPurePath, right: IPurePath) =
+        left.Join right
+

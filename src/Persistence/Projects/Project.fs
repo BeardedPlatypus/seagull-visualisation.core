579bf31cc92253479f4eb968b185c8b9f6b1fe52
BeardedPlatypus <mwtegelaers@gmail.com>
2021-05-31 14:26:41 +0200

    Add ProjectServiceJson implementation

---
+++ src/Persistence/Projects/Project.fs
@@ -0,0 +1,23 @@
+﻿namespace Seagull.Visualisation.Core.Persistence.Projects
+
+open Seagull.Visualisation.Core
+open Seagull.Visualisation.Core.Persistence.Utilities
+open PathLib
+
+module internal Project =
+    type Version =
+        { Major : int
+          Minor : int
+          Patch : int }
+        static member FromDomain (version: Domain.Projects.ProjectVersion): Version =
+            { Major = version.Major
+              Minor = version.Minor
+              Patch = version.Patch }
+    
+    type T =
+        { Version : Version }
+        
+    let createNewProject (path: IPath) : unit =
+        { Version = Version.FromDomain Domain.Projects.ProjectVersion.CurrentVersion }
+        |> Json.toObject
+        |> Json.writeObj path
---
+++ src/Persistence/Projects/ProjectServiceJson.fs
@@ -0,0 +1,10 @@
+﻿namespace Seagull.Visualisation.Core.Persistence.Projects
+
+open Seagull.Visualisation.Core.Application
+
+open PathLib
+
+type ProjectServiceJson  =
+    interface IProjectService with
+        member this.CreateProject(newProjectPath: IPath) : unit =
+            Project.createNewProject newProjectPath

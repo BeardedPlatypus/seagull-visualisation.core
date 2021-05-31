59b26684530e0e38695664f7cf215cc39f4d2951
BeardedPlatypus <mwtegelaers@gmail.com>
2021-05-31 21:51:14 +0200

    Add ProjectJsonTest with CreateProject test

---
+++ test/Persistence.Tests/Projects/ProjectJsonTest.fs
@@ -0,0 +1,27 @@
+﻿namespace Seagull.Visualisation.Core.Persistence.Tests
+
+open NUnit.Framework
+open FsUnit
+
+open Seagull.Visualisation.Core
+open Seagull.Visualisation.Core.Persistence
+open Seagull.Visualisation.Core.Persistence.Projects
+open Seagull.Visualisation.Core.Persistence.Projects.Project
+
+[<TestFixture>]
+type ProjectJsonTest () =
+    [<Test>]
+    member this.``Creating a project should create a correct file on disk. `` () =
+        use tempDir = new TemporaryDirectory ()
+        let filePath = tempDir.Path.Join "test.seagull"
+        
+        ProjectJson.createNewProject filePath
+
+        let readResult =
+            Utilities.Json.getObject filePath
+            |> Utilities.Json.deserialize<Project.T>
+
+        readResult |> should equal { Version = Version.FromDomain Domain.Projects.ProjectVersion.CurrentVersion }
+
+
+        
---
+++ test/Persistence.Tests/TemporaryDirectory.fs
@@ -0,0 +1,27 @@
+﻿namespace Seagull.Visualisation.Core.Persistence.Tests
+
+open System
+open PathLib
+
+[<Sealed>]
+type TemporaryDirectory () =
+    let tempDirectoryPath =
+        Paths.Create(System.IO.Path.GetTempPath(), System.IO.Path.GetRandomFileName())
+    
+    do tempDirectoryPath.Mkdir(true)
+    
+    let mutable hasDisposed = false
+    let removeDirectory () =
+        System.IO.Directory.Delete(tempDirectoryPath.ToString(), true)
+    
+    member this.Path = tempDirectoryPath
+    
+    interface IDisposable with
+        
+        member this.Dispose() =
+            if not hasDisposed then
+                removeDirectory ()
+                hasDisposed = true |> ignore
+            else
+                ()
+            

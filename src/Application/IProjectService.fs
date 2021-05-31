27f3ad682937617795088433cd4180cd89d34831
BeardedPlatypus <mwtegelaers@gmail.com>
2021-05-31 14:26:01 +0200

    Add IProjectService

---
+++ src/Application/IProjectService.fs
@@ -0,0 +1,8 @@
+﻿namespace Seagull.Visualisation.Core.Application
+
+open PathLib
+open Seagull.Visualisation.Core.Domain.Projects
+
+[<Interface>]
+type public IProjectService =
+    abstract member CreateProject : IPath -> unit 

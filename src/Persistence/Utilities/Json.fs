afb7d55cb8d2fb872f684993903901455838c860
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-05 12:28:24 +0200

    Extend Persistence.Utilities.Json with new object functions

--- src/Persistence/Utilities/Json.fs
+++ src/Persistence/Utilities/Json.fs
@@ -11,8 +11,10 @@ open Newtonsoft.Json.Linq
 /// The <see cref="Json"/> module provides the methods to handle Json files.
 /// </summary>
 module internal Json =
-    let internal query (propertyPath: string) (obj: JToken) =
-        obj.SelectToken propertyPath
+    let internal query (propertyPath: string) (obj: JToken) : Option<JToken> =
+        match obj.SelectToken propertyPath with 
+        | null -> None
+        | v    -> Some v
         
     let internal getObject (path: IPath) : JToken =
         use sr = new StreamReader (path.Open FileMode.Open)
@@ -25,9 +27,35 @@ module internal Json =
         
     let internal update<'T> (obj: JToken) (path: string) (value: 'T) : unit =
         obj.Replace (JToken.FromObject value)
+
+    module internal Util =
+        let internal splitLastSection (path: string) : string * string =
+            let index = path.LastIndexOf "."
+            path.Substring(0, path.Length - index - 1), path.Substring(index)
+
+        let rec internal addRec (obj: JObject) (path: string) (toAdd: JToken) : unit =
+            if path.Length > 0 then 
+                match query path obj with 
+                | Some v -> 
+                    (v :?> JObject).Add toAdd
+                | None   ->  
+                    let newPath, toAddParent = splitLastSection path
+
+                    let newToAdd = JObject()
+                    newToAdd.Add(toAddParent, toAdd)
+
+                    addRec obj newPath newToAdd
+            else 
+                obj.Add toAdd
+
+    let internal add<'T> (obj: JToken) (path: string) (value: 'T) : unit =
+        // Assumption: paths do not contain indices of arrays (e.g. SomeValue[0])
+        Util.addRec (obj :?> JObject) path (toObject value)
         
     let internal remove<'T> (obj: JToken) (path: string) : unit =
-        (query path obj).Remove()
+        match query path obj with 
+        | Some v -> v.Remove()
+        | None   -> ()
 
     let internal deserialize<'T> (obj: JToken) =
         obj.ToObject<'T>()
@@ -39,3 +67,5 @@ module internal Json =
         use jsonWriter = new JsonTextWriter(sw)
         
         obj.WriteTo(jsonWriter)
+
+    let internal emptyObj () : JToken = JObject() :> JToken

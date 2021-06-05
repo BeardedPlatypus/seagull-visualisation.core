6c63a4b50e3d0859d1a9bdf7091c26ad3139d973
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-05 12:31:08 +0200

    Refactor RecentProjectService to utilise new IAppDataRepository

--- src/Persistence/RecentProjects/RecentProjectServiceJson.fs
+++ src/Persistence/RecentProjects/RecentProjectServiceJson.fs
@@ -11,33 +11,25 @@ type RecentProjectServiceJson (appDataRepository: IAppDataRepository) =
     let appDataRepository = appDataRepository
     
     let getRecentProjects () =
-        appDataRepository.Query<seq<RecentProject.T>> RecentProject.Key
+        appDataRepository.Query RecentProject.Key Seq.empty
         
     let writeRecentProjects (recentProjects: seq<RecentProject.T>) : unit =
         appDataRepository.Write RecentProject.Key recentProjects
         
     interface Application.IRecentProjectsService with
-        member this.GetRecentProjects () =
-            match getRecentProjects () with
-            | Result.Ok res  -> Seq.map (fun (v: RecentProject.T) -> v.ToDomain()) res
-            | Result.Error _ -> Seq.empty
+        member this.GetRecentProjects () = 
+            getRecentProjects ()
             
         member this.UpdateRecentProject(recentProject) =
             let rpValue = RecentProject.T.FromDomain recentProject
             
-            match getRecentProjects () with
-            | Result.Ok res  ->
-                res
-                |> RecentProject.updateRecentProjects rpValue
-                |> writeRecentProjects
-            | Result.Error _ -> () 
+            getRecentProjects ()
+            |> RecentProject.updateRecentProjects rpValue
+            |> writeRecentProjects
         
         member this.RemoveRecentProject(recentProject) =
             let rpValue = RecentProject.T.FromDomain recentProject
 
-            match getRecentProjects () with
-            | Result.Ok res  ->
-                res
-                |> RecentProject.removeRecentProject rpValue
-                |> writeRecentProjects
-            | Result.Error _ -> () 
+            getRecentProjects ()
+            |> RecentProject.removeRecentProject rpValue
+            |> writeRecentProjects

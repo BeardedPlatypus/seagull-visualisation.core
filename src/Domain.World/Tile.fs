b336d9f5a5e683a4a50b0b7165ebc0f7c2347ff5
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-06 17:26:34 +0200

    Add tile module to Seagull.Visualisation.Core.Domain.World

---
+++ src/Domain.World/Tile.fs
@@ -0,0 +1,34 @@
+﻿namespace Seagull.Visualisation.Core.Domain.World
+
+/// <summary>
+/// The <see cref="Tile"/> module defines all concepts related to working
+/// with tiles.
+/// </summary>
+[<RequireQualifiedAccess>]
+module public Tile = 
+    [<RequireQualifiedAccess>]
+    [<Struct>]
+    type public Index = 
+        { X: int 
+          Y: int 
+          ZoomLevel: int }
+
+    /// <summary>
+    /// <see cref="Image"/> defines an image as used within the TileSource
+    /// library.
+    /// </summary>
+    type public Image = 
+        | Image of byte[]
+    
+        /// <summary>
+        /// Retrieve the underlying bytes array 
+        /// </summary>
+        /// <returns>
+        /// The bytes array corresponding with this <see cref="Image"/>.
+        /// </returns>
+        member self.toBytes () = match self with | Image b -> b
+
+    type public Source =
+        | Source of string
+        member x.ToStringRepr () = match x with | Source s -> s
+

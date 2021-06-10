576dffa80c66e23e4eec02ad0973a6d228e90d14
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-10 23:00:24 +0200

    Document IProjectService

--- src/Application/IProjectService.fs
+++ src/Application/IProjectService.fs
@@ -1,8 +1,18 @@
 ﻿namespace Seagull.Visualisation.Core.Application
 
 open PathLib
-open Seagull.Visualisation.Core.Domain.Projects
 
+/// <summary>
+/// <see cref="IProjectService"/> defines the interface with which to create
+/// and manipulate seagull projects
+/// </summary>
 [<Interface>]
 type public IProjectService =
-    abstract member CreateProject : IPath -> unit 
+    /// <summary>
+    /// Create a project given a path to the new manifest file location.
+    /// </summary>
+    /// <param name="manifestPath">The path to the manifest file.</param>
+    /// <remarks>
+    /// <paramref name="manifestPath"/> is supposed to be a file path.
+    /// </remarks>
+    abstract member CreateProject : manifestPath:IPath -> unit 

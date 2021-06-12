8bdddc20d2a9e06d3b1bb88a07feb76dca18cc2d
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-12 21:58:36 +0200

    Add RemoteBehaviourNone

---
+++ src/Infrastructure.World/RemoteBehaviourNone.fs
@@ -0,0 +1,18 @@
+﻿namespace Seagull.Visualisation.Core.Persistence.World
+
+open Seagull.Visualisation.Core.Domain.World
+open Seagull.Visualisation.Core.Application.World
+
+/// <summary>
+/// <see cref="RemoteBehaviourNone"/> implements the <see cref="IRemoteBehaviour"/>
+/// to raise a not supported exception.
+/// </summary>
+[<Sealed>]
+type public RemoteBehaviourNone =
+    interface IRemoteBehaviour with 
+        member _.RetrieveTile (_: Tile.Index) : Tile.Image = 
+            raise <| System.NotSupportedException($"Cannot retrieve tiles with a {nameof(RemoteBehaviourNone)}")
+
+    interface System.IDisposable with
+        member this.Dispose() = ()
+  

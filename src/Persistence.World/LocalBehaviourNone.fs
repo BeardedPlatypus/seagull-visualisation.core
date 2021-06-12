58f5d77679b33be8d57710d1688fb967a3ee6d57
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-12 21:52:28 +0200

    Add LocalBehaviourNone

---
+++ src/Persistence.World/LocalBehaviourNone.fs
@@ -0,0 +1,18 @@
+﻿namespace Seagull.Visualisation.Core.Persistence.World
+
+open Seagull.Visualisation.Core.Domain.World
+open Seagull.Visualisation.Core.Application.World
+
+/// <summary>
+/// <see cref="LocalBehaviourNone"/> implements the <see cref="ILocalBehaviour"/>
+/// to always return none.
+/// </summary>
+[<Sealed>]
+type public LocalBehaviourNone =
+    interface ILocalBehaviour with 
+        member _.RetrieveTile (_: Tile.Index) : Tile.Image Option = 
+            None
+
+        member _.StoreTile (_: Tile.Index) (_: Tile.Image) : unit =
+            ()
+  

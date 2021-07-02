c23d98f4f5b9f9a494aa4d1b85c1fe8d90697a19
BeardedPlatypus <mwtegelaers@gmail.com>
2021-07-02 22:17:56 +0200

    Add Face2D to Domain.Model

---
+++ src/Domain.Model/Grid/Face2D.fs
@@ -0,0 +1,19 @@
+﻿namespace Seagull.Visualisation.Core.Domain.Model
+
+/// <summary>
+/// <see cref="Face2D"/> defines a two-dimensional face, where the indices
+/// refer to two vertices within a mesh.
+/// </summary>
+/// <remarks>
+/// <see cref="Indices"/> are expected to be distinct and in counter-clockwise 
+/// order.
+/// </remarks>
+[<Sealed>]
+type public Face2D (indices: seq<int>) =
+    let indices: int list = indices |> List.ofSeq
+
+    /// <summary>
+    /// Gets the indices of this <see cref="Face2D"/>
+    /// </summary>
+    member public this.Indices: seq<int> = indices |> Seq.ofList
+

29455912dfb2ef78e106f3413d16fa3a4d179542
BeardedPlatypus <mwtegelaers@gmail.com>
2021-07-02 22:37:36 +0200

    Add IMesh1D to Domain.Model

---
+++ src/Domain.Model/IMesh1D.fs
@@ -0,0 +1,36 @@
+﻿namespace Seagull.Visualisation.Core.Domain.Model
+
+/// <summary>
+/// <see cref="IMesh1D"/> defines a one-dimensional network consisting of
+/// vertices (nodes) and edges connecting these vertices.
+/// </summary>
+[<Interface>]
+type public IMesh1D =
+    /// <summary>
+    /// Retrieve the name of this <see cref="IMesh1D"/>.
+    /// </summary>
+    /// <returns>
+    /// The name of this <see cref="IMesh1D"/>.
+    /// </returns>
+    abstract member RetrieveName: unit -> string
+
+    /// <summary>
+    /// Retrieve the vertices of this <see cref="IMesh1D"/>.
+    /// </summary>
+    /// <returns>
+    /// The vertices of this <see cref="IMesh1D"/>.
+    /// </returns>
+    abstract member RetrieveVertices: unit -> seq<Grid.Vertex2D>
+
+    /// <summary>
+    /// Retrieve the edges of this <see cref="IMesh1D"/>.
+    /// </summary>
+    /// <returns>
+    /// The edges of this <see cref="IMesh1D"/>.
+    /// </returns>
+    /// <remarks>
+    /// Note that the indices of the <see cref="Grid.Edge2D"/> refer to the
+    /// vertices of <see cref="RetrieveVertices"/>.
+    /// </remarks>
+    abstract member RetrieveEdges: unit -> seq<Grid.Edge2D>
+

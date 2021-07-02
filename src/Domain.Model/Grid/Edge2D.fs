a890898edb1837111f16629549c40c4611f7b064
BeardedPlatypus <mwtegelaers@gmail.com>
2021-07-02 22:11:24 +0200

    Add Edge2D

---
+++ src/Domain.Model/Grid/Edge2D.fs
@@ -0,0 +1,21 @@
+﻿namespace Seagull.Visualisation.Core.Domain.Model
+
+/// <summary>
+/// <see cref="Edge2D"/> defines a two-dimensional edge, where the indices
+/// refer to two vertices within a mesh.
+/// </summary>
+/// <remarks>
+/// <see cref="IndexA"/> and <see cref="IndexB"/> are expected to be different.
+/// </remarks>
+[<Sealed>]
+type public Edge2D (indexA: int, indexB: int) =
+    /// <summary>
+    /// The first index of this <see cref="Edge2D"/>.
+    /// </summary>
+    member val public IndexA: int = indexA
+
+    /// <summary>
+    /// The second index of this <see cref="Edge2D"/>.
+    /// </summary>
+    member val public IndexB: int = indexB
+

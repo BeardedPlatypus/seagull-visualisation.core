b460aafe0303fbc50549f745a813e8f7d27f7ef2
BeardedPlatypus <mwtegelaers@gmail.com>
2021-07-02 22:28:30 +0200

    Add Extents to Domain.Model.Grid

---
+++ src/Domain.Model/Grid/Extents.fs
@@ -0,0 +1,32 @@
+﻿namespace Seagull.Visualisation.Core.Domain.Model.Grid
+
+/// <summary>
+/// <see cref="Extents"/> defines a rectangle with a min and max value in both
+/// the x and y directions.
+/// </summary>
+/// <remarks>
+/// It is expected that xMin is smaller or equal to xMax and yMin is smaller 
+/// or equal to yMax.
+/// </remarks>
+[<Sealed>]
+type public Extents (xMin: double, xMax: double, yMin: double, yMax: double) =
+    /// <summary>
+    /// Gets the minimum x value.
+    /// </summary>
+    member val public XMin: double = xMin;
+
+    /// <summary>
+    /// Gets the maximum x value.
+    /// </summary>
+    member val public XMax: double = xMax;
+
+    /// <summary>
+    /// Gets the minimum y value.
+    /// </summary>
+    member val public YMin: double = yMin;
+
+    /// <summary>
+    /// Gets the maximum y value.
+    /// </summary>
+    member val public YMax: double = yMax;
+

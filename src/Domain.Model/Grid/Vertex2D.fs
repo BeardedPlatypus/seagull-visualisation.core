6292078623c8fc75fbef8e70591732edc602df8e
BeardedPlatypus <mwtegelaers@gmail.com>
2021-07-02 16:29:22 +0200

    Add Vertex2D

---
+++ src/Domain.Model/Grid/Vertex2D.fs
@@ -0,0 +1,18 @@
+﻿namespace Seagull.Visualisation.Core.Domain.Model
+
+/// <summary>
+/// <see cref="Vertex2D"/> defines a two dimensional vertex consisting of a x 
+/// and y coordinate.
+/// </summary> 
+[<Sealed>]
+type public Vertex2D (x: double, y: double) =
+    /// <summary>
+    /// Gets the X-coordinate of this <see cref="Vertex2D"/>
+    /// </summary>
+    member val public X: double = x with get
+
+    /// <summary>
+    /// Gets the Y-coordinate of this <see cref="Vertex2D"/>
+    /// </summary>
+    member val public Y: double = y with get
+

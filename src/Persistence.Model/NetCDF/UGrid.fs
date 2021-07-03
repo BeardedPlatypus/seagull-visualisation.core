1f4ca0835a8c07e3f2065817faa364fc46d2363d
BeardedPlatypus <mwtegelaers@gmail.com>
2021-07-03 17:19:38 +0200

    Add UGrid module to Persistence.Model

---
+++ src/Persistence.Model/NetCDF/UGrid.fs
@@ -0,0 +1,14 @@
+﻿namespace Seagull.Visualisation.Core.Persistence.Model.NetCDF
+
+open BeardedPlatypus.Functional.NetCDF
+open PathLib
+
+/// <summary>
+/// The <see cref="UGrid"/> module provides common methods to
+/// query a NetCDF file with UGrid conventions and retrieve
+/// the relevant model data.
+/// </summary>
+module internal UGrid =
+    let internal query (path: IPath) (query: IQuery) =
+        Service.Query (path.ToString (), query)
+

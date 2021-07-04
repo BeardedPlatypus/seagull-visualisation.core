4057a9234105eb73a32400c691032a4012e0006d
BeardedPlatypus <mwtegelaers@gmail.com>
2021-07-04 13:11:42 +0200

    Add UGrid.RetrieveMeshesQuery to Persistence.Model

--- src/Persistence.Model/NetCDF/UGrid.fs
+++ src/Persistence.Model/NetCDF/UGrid.fs
@@ -1,7 +1,9 @@
-namespace Seagull.Visualisation.Core.Persistence.Model.NetCDF
+﻿namespace Seagull.Visualisation.Core.Persistence.Model.NetCDF
 
 open BeardedPlatypus.Functional.NetCDF
 open PathLib
+
+open Seagull.Visualisation.Core.Domain.Model
 
 /// <summary>
 /// The <see cref="UGrid"/> module provides common methods to
@@ -9,7 +11,7 @@ open PathLib
 /// the relevant model data.
 /// </summary>
 module internal UGrid =
-    let internal query (path: IPath) (query: IQuery) =
+    let internal executeQuery (path: IPath) (query: IQuery) =
         Service.Query (path.ToString (), query)
 
     [<Sealed>]
@@ -99,4 +101,58 @@ module internal UGrid =
                     |> Seq.map buildFace
 
                 result <- faces
+
+    [<Sealed>]
+    type internal RetrieveMeshesQuery (netCDFPath: IPath) = 
+        let getDimensionMesh (repository: IRepository) (meshID: VariableID): int =
+            (repository.RetrieveVariableAttribute (meshID, "topology_dimension")).Values
+            |> Seq.head
+
+        let retrieveVertices (meshID: VariableID): unit -> seq<Grid.Vertex2D> = 
+            fun () -> 
+                let query = RetrieveVerticesQuery meshID
+                executeQuery netCDFPath query
+                query.Result
+
+        let retrieveEdges (meshID: VariableID): unit -> seq<Grid.Edge2D> =
+            fun () ->
+                let query = RetrieveEdgesQuery meshID
+                executeQuery netCDFPath query 
+                query.Result
+
+        let retrieveFaces (meshID: VariableID): unit -> seq<Grid.Face2D> =
+            fun () ->
+                let query = RetrieveFacesQuery meshID
+                executeQuery netCDFPath query 
+                query.Result
+
+        let createMesh1D (repository: IRepository) (meshID: VariableID): IMesh1D =
+            let name = repository.RetrieveVariableName meshID
+            Mesh1D(name, retrieveVertices meshID, retrieveEdges meshID)
+            :> IMesh1D
+
+        let createMesh2D (repository: IRepository) (meshID: VariableID): IMesh2D =
+            let name = repository.RetrieveVariableName meshID
+            Mesh2D(name, retrieveVertices meshID, retrieveEdges meshID, retrieveFaces meshID)
+            :> IMesh2D
+
+        let mutable meshes1D: seq<IMesh1D> = Seq.empty
+        let mutable meshes2D: seq<IMesh2D> = Seq.empty
+
+        member val Meshes1D: seq<IMesh1D> = meshes1D with get
+        member val Meshes2D: seq<IMesh2D> = meshes2D with get
+
+
+        interface IQuery with 
+            member this.Execute (repository: IRepository) : unit =
+                let meshIds : seq<VariableID> = repository.RetrieveVariablesWithAttributeWithValue ("cf_role", "mesh_topology")
+
+                for (dim, ids) in meshIds |> Seq.groupBy (getDimensionMesh repository) do
+                    match dim with 
+                    | 1 -> 
+                        meshes1D <- Seq.map (createMesh1D repository) ids
+                    | 2 -> 
+                        meshes2D <- Seq.map (createMesh2D repository) ids
+                    | _ ->
+                        ()
 

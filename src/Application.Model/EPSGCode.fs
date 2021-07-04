c0502b966327cb7eae02f5fcb1e50b8bb69ba0c3
BeardedPlatypus <mwtegelaers@gmail.com>
2021-07-04 14:46:04 +0200

    Add ICoordinateSystemTransformationService to Application.Model

---
+++ src/Application.Model/EPSGCode.fs
@@ -0,0 +1,22 @@
+﻿namespace Seagull.Visualisation.Core.Application.Model
+
+/// <summary>
+/// <see cref="EPSGCode"/> defines a single epsg code used to transform 
+/// coordinates from coordinate system to another.
+/// </summary>
+[<Struct>]
+type public EPSGCode =
+    struct 
+        /// <summary>
+        /// The ID of this <see cref="EPSGCode"/>.
+        /// </summary>
+        val ID: int
+
+        /// <summary>
+        /// Create a new <see cref="EPSGCode"/> with the given 
+        /// <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">The ID of the new epsg code</param>
+        new(id: int) = { ID = id }
+    end
+
---
+++ src/Application.Model/ICoordinateSystemTransformationService.fs
@@ -0,0 +1,32 @@
+﻿namespace Seagull.Visualisation.Core.Application.Model
+
+/// <summary>
+/// <see cref="ICoordinateSystemTransformationService"/> defines the 
+/// interface with which to convert coordinates from one coordinate system
+/// to another.
+/// </summary>
+[<Interface>]
+type public ICoordinateSystemTransformationService =
+    /// <summary>
+    /// Convert the specified coordinate with x and y coordinate values
+    /// in the <paramref name="fromCoordinateSystem"/> to the corresponding
+    /// coordinates in the <paramref name="toCoordinateSystem"/>
+    /// </summary>
+    /// <param name="toCoordinateSystem">
+    /// The coordinate system to which the coordinates should be transformed.
+    /// </param>
+    /// <param name="fromCoordinateSystem">
+    /// The coordinate system from which the coordinates should be transformed.
+    /// </param>
+    /// <param name="x">The x-coordinate.</param>
+    /// <param name="y">The x-coordinate.</param>
+    /// <returns>
+    /// The coordinates in the <paramref name="toCoordinateSystem"/>
+    /// corresponding with the provided coordinates in the 
+    /// <paramref name="fromCoordinateSystem"/>.
+    /// </returns>
+    abstract member Convert: toCoordinateSystem: EPSGCode ->
+                             fromCoordinateSystem: EPSGCode ->
+                             x: double * y: double -> 
+                             double * double
+

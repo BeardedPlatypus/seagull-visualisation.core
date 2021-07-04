654949aea4d18c69a386d89d02ab02490b00fc7c
BeardedPlatypus <mwtegelaers@gmail.com>
2021-07-04 12:41:35 +0200

    Add Mesh1D implementation to Persistence.Model.NetCDF

---
+++ src/Persistence.Model/NetCDF/Mesh1D.fs
@@ -0,0 +1,17 @@
+﻿namespace Seagull.Visualisation.Core.Persistence.Model.NetCDF
+
+open Seagull.Visualisation.Core.Domain.Model
+
+[<Sealed>]
+type internal Mesh1D (name: string, 
+                      queryVertices: unit -> seq<Grid.Vertex2D>,
+                      queryEdges: unit -> seq<Grid.Edge2D>) =
+    interface IMesh1D with
+        member this.RetrieveName (): string = name
+ 
+        member this.RetrieveVertices(): seq<Grid.Vertex2D> = 
+            queryVertices ()
+ 
+        member this.RetrieveEdges(): seq<Grid.Edge2D> = 
+            queryEdges()
+

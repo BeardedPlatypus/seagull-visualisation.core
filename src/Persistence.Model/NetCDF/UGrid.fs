a6542c4e2bf0858f757aba0db50d1e290ee01067
BeardedPlatypus <mwtegelaers@gmail.com>
2021-07-04 13:04:39 +0200

    Add UGrid.RetrieveVerticesQuery to Persistence.Model

--- src/Persistence.Model/NetCDF/UGrid.fs
+++ src/Persistence.Model/NetCDF/UGrid.fs
@@ -1,4 +1,4 @@
-﻿namespace Seagull.Visualisation.Core.Persistence.Model.NetCDF
+namespace Seagull.Visualisation.Core.Persistence.Model.NetCDF
 
 open BeardedPlatypus.Functional.NetCDF
 open PathLib
@@ -11,4 +11,39 @@ open PathLib
 module internal UGrid =
     let internal query (path: IPath) (query: IQuery) =
         Service.Query (path.ToString (), query)
+
+    [<Sealed>]
+    type internal RetrieveVerticesQuery (meshID: VariableID) = 
+        let mutable result : seq<Grid.Vertex2D> = Seq.empty
+
+        let getVertexVariableNames (repository: IRepository) : string[] =
+            let vertexVariableAttribute: string = 
+                (repository.RetrieveVariableAttribute<string>(meshID, "node_coordinates")).Values
+                |> Seq.head
+
+            vertexVariableAttribute.Split([|" "|], System.StringSplitOptions.RemoveEmptyEntries)
+
+        let getStandardName (repository: IRepository) (variableName: string) =
+            (repository.RetrieveVariableAttribute(variableName, "standard_name")).Values
+            |> Seq.head
+
+        let getCoordinateData (repository: IRepository) (coordinateIdentifier: string) (variableNames: string[]): seq<double> =
+            let isCoordinateData (variableName: string): bool =
+                (getStandardName repository variableName) = coordinateIdentifier
+
+            let coordinateVariableName = Array.find isCoordinateData variableNames
+            (repository.RetrieveVariableValue(coordinateVariableName)).Values
+
+        member val Result = result with get
+
+        interface IQuery with
+            member this.Execute (repository: IRepository) : unit = 
+                let vertexVariableNames = getVertexVariableNames repository
+
+                let xVertexData = getCoordinateData repository "projection_x_coordinate" vertexVariableNames
+                let yVertexData = getCoordinateData repository "projection_y_coordinate" vertexVariableNames
+
+                // TODO add conversion here
+                let vertices = Seq.map2 (fun x y -> Grid.Vertex2D(x, y)) xVertexData yVertexData
+                result <- vertices
 
